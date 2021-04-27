c4ad872df5fe9aa4c33d30ed9858386eb778f857
Muasya <francismuasya@gmail.com>
2021-04-27 17:21:33 +0300

    define transaction and blockhead structures

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Rust Blockchain
+
+This is a simple blockchain implementation using Rust Lang.
---
+++ src/blockchain.rs
@@ -0,0 +1,23 @@
+extern crate serde;
+extern crate serde_json;
+extern crate sha2;
+extern crate time;
+
+use serde::Serialize;
+use sha2::{Digest, Sha256};
+use std::fmt::Write;
+
+#[derive(Debug, Clone, Serialize)]
+struct Transaction {
+    sender: String,
+    receiver: String,
+    amount: f32,
+}
+
+pub struct Blockheader {
+    timestamp: i64,
+    nonce: u32,
+    pre_hash: String,
+    merkle: String,
+    difficulty: u32,
+}
